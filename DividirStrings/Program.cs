73f32c5e8e5a1e4c997e69e7b8407355343adf1e
Leonardo Urbano <44528586+LeonardoHenriq@users.noreply.github.com>
2018-10-29 23:40:49 -0300

    Add files via upload

---
+++ DividirStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DividirStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dados, nome , end, fone, email;
+            int pos = 0;
+
+            Console.WriteLine("Informe Nome, Endereço, Telefone e E-mail: ");
+            dados = Console.ReadLine();
+
+            for (int i = 0; i < dados.Length; i++)
+            {
+                int index = dados.IndexOf(';');
+
+                nome =dados.Substring(dados[i], index);
+                
+            }
+            
+
+
+        }
+    }
+}
