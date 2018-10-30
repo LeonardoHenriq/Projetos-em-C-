b42de042245d4d106d27662ce2c4b932a437190e
Leonardo Urbano <44528586+LeonardoHenriq@users.noreply.github.com>
2018-10-29 23:37:47 -0300

    Add files via upload

---
+++ cadastroCliente/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace cadastroCliente
+{
+    partial class Cadastro_Cliente
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cadastro_Cliente));
+            this.btnvalidar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtend = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.Nome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtrg = new System.Windows.Forms.MaskedTextBox();
+            this.txtcpf = new System.Windows.Forms.MaskedTextBox();
+            this.txtnascimento = new System.Windows.Forms.MaskedTextBox();
+            this.txtfone = new System.Windows.Forms.MaskedTextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnvalidar
+            // 
+            this.btnvalidar.Location = new System.Drawing.Point(119, 203);
+            this.btnvalidar.Name = "btnvalidar";
+            this.btnvalidar.Size = new System.Drawing.Size(60, 23);
+            this.btnvalidar.TabIndex = 8;
+            this.btnvalidar.Text = "validar";
+            this.btnvalidar.UseVisualStyleBackColor = true;
+            this.btnvalidar.Click += new System.EventHandler(this.btnvalidar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(100, 21);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(163, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtend
+            // 
+            this.txtend.Location = new System.Drawing.Point(100, 125);
+            this.txtend.Name = "txtend";
+            this.txtend.Size = new System.Drawing.Size(163, 20);
+            this.txtend.TabIndex = 5;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(100, 177);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(163, 20);
+            this.txtemail.TabIndex = 7;
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(59, 21);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(35, 13);
+            this.Nome.TabIndex = 7;
+            this.Nome.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "RG";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "CPF";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "nascimento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Endereço";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "E-mail";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Fone";
+            // 
+            // txtrg
+            // 
+            this.txtrg.Location = new System.Drawing.Point(100, 47);
+            this.txtrg.Mask = "00.000.000-A";
+            this.txtrg.Name = "txtrg";
+            this.txtrg.Size = new System.Drawing.Size(163, 20);
+            this.txtrg.TabIndex = 2;
+            // 
+            // txtcpf
+            // 
+            this.txtcpf.Location = new System.Drawing.Point(100, 73);
+            this.txtcpf.Mask = "000.000.000-A";
+            this.txtcpf.Name = "txtcpf";
+            this.txtcpf.Size = new System.Drawing.Size(163, 20);
+            this.txtcpf.TabIndex = 3;
+            // 
+            // txtnascimento
+            // 
+            this.txtnascimento.Location = new System.Drawing.Point(100, 99);
+            this.txtnascimento.Mask = "00/00/0000";
+            this.txtnascimento.Name = "txtnascimento";
+            this.txtnascimento.Size = new System.Drawing.Size(163, 20);
+            this.txtnascimento.TabIndex = 4;
+            // 
+            // txtfone
+            // 
+            this.txtfone.Location = new System.Drawing.Point(100, 151);
+            this.txtfone.Mask = "(99)00000-0000";
+            this.txtfone.Name = "txtfone";
+            this.txtfone.Size = new System.Drawing.Size(163, 20);
+            this.txtfone.TabIndex = 6;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(197, 203);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(66, 23);
+            this.btnbuscar.TabIndex = 16;
+            this.btnbuscar.Text = "buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // Cadastro_Cliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.txtfone);
+            this.Controls.Add(this.txtnascimento);
+            this.Controls.Add(this.txtcpf);
+            this.Controls.Add(this.txtrg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtend);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnvalidar);
+            this.Name = "Cadastro_Cliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnvalidar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtend;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txtrg;
+        private System.Windows.Forms.MaskedTextBox txtcpf;
+        private System.Windows.Forms.MaskedTextBox txtnascimento;
+        private System.Windows.Forms.MaskedTextBox txtfone;
+        private System.Windows.Forms.Button btnbuscar;
+    }
+}
+
---
+++ cadastroCliente/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cadastroCliente
+{
+    public partial class Cadastro_Cliente : Form
+    {
+        public Cadastro_Cliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnvalidar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text.Trim() == "")
+            {
+                MessageBox.Show("por favor preencha o campo nome!!!");
+                txtNome.Text = "";
+                txtNome.Focus();
+            }
+            else if (txtrg.MaskFull == false)
+            {
+                MessageBox.Show("por favor preencha o campo rg!!!");
+                txtrg.Text = "";
+                txtrg.Focus();
+            }
+            else if (txtcpf.MaskFull == false)
+            {
+                MessageBox.Show("por favor preencha o campo cpf!!!");
+                txtcpf.Text = "";
+                txtcpf.Focus();
+            }
+            else if (txtnascimento.MaskFull == false)
+            {
+                MessageBox.Show("por favor preencha o campo Nascimento!!!");
+                txtnascimento.Text = "";
+                txtnascimento.Focus();
+            }
+            else if (txtend.Text.Trim() == "")
+            {
+                MessageBox.Show("por favor preencha o campo Endereço!!!");
+                txtend.Text = "";
+                txtend.Focus();
+            }
+            else if (txtfone.MaskFull == false)
+            {
+                MessageBox.Show("por favor preencha o campo Fone!!!");
+                txtfone.Text = "";
+                txtfone.Focus();
+            }
+            else if (txtemail.Text.Trim() == "")
+            {
+                MessageBox.Show("por favor preencha o campo E-mail!!!");
+                txtemail.Text = "";
+                txtemail.Focus();
+            }
+            else
+            {
+
+                MessageBox.Show("\n Nome: " + txtNome.Text.Trim() + "\n RG: " + txtrg.Text.Trim() + "\n CPF: " + txtcpf.Text.Trim() + "\n Nascimento: " + txtnascimento.Text.Trim() + "\n Endereço: " + txtend.Text.Trim() + "\n Fone: " + txtfone.Text.Trim() + "\n E-mail: " + txtemail.Text.Trim());
+
+                StreamWriter arq = new StreamWriter(@"C:\Users\leonardo\Desktop\Registro_de_Clientes.txt", true);
+                arq.WriteLine(txtNome.Text.Trim());
+                arq.WriteLine(txtrg.Text.Trim());
+                arq.WriteLine(txtcpf.Text.Trim());
+                arq.WriteLine(txtnascimento.Text.Trim());
+                arq.WriteLine(txtend.Text.Trim());
+                arq.WriteLine(txtfone.Text.Trim());
+                arq.WriteLine(txtemail.Text.Trim());
+                arq.Dispose();
+                arq.Close();
+
+                txtNome.Text = "";
+                txtrg.Text = "";
+                txtcpf.Text = "";
+                txtnascimento.Text = "";
+                txtend.Text = "";
+                txtfone.Text = "";
+                txtemail.Text = "";
+            }
+            
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("informe quem deseja buscar");
+            
+            //txtNome.Focus();
+            string busca = txtNome.Text;
+            bool achou = false;
+            StreamReader ler = new StreamReader(@"C:\Users\leonardo\Desktop\Registro_de_Clientes.txt");
+
+            string linha = "";
+            while ((linha = ler.ReadLine())!= null)
+            {
+                if (linha == busca)
+                {
+                    MessageBox.Show("nome: " + linha + "\n RG: " + ler.ReadLine() + "\n CPF: " + ler.ReadLine() + "\n Data de nascimento: " + ler.ReadLine() + "\n Endereço: " + ler.ReadLine() + "\n Telefone: "+ ler.ReadLine()+"\n E-mail: " +ler.ReadLine());
+
+                    txtNome.Text =busca;
+                    txtrg.Text = ler.ReadLine();
+                    txtcpf.Text = ler.ReadLine();
+                    txtnascimento.Text = ler.ReadLine();
+                    txtend.Text = ler.ReadLine();
+                    txtfone.Text = ler.ReadLine();
+                    txtemail.Text = ler.ReadLine();
+
+                    achou = true;
+                    break;
+                }
+                
+            }
+            if (achou == false)
+            {
+                MessageBox.Show(busca + " nao encontrado");
+            }
+            ler.Dispose();
+            ler.Close();
+
+        }
+    }
+}
