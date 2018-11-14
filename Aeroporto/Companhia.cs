bdc6d47e18c80ddf03fd2cc3f01a16269b4dfb67
Leonardo Urbano <44528586+LeonardoHenriq@users.noreply.github.com>
2018-11-14 06:20:31 -0200

    Usando o Padrao Singleton
    Implementaçao do Padrao de Projeto Singleton

---
+++ Aeroporto/Companhia.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aeroporto
+{
+    public class Companhia
+    {
+        private static Companhia _instancia;
+        private List<string> nomes = new List<string>();
+        private Random _random = new Random();
+
+        private static object syncLock = new object();
+        protected Companhia()
+        {
+            nomes.Add("Gol");
+            nomes.Add("Tam");
+            nomes.Add("Airline");
+            nomes.Add("Latam");
+
+        }
+
+        public static Companhia GetCompanhia()
+        {
+            if (_instancia == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instancia == null)
+                    {
+                        _instancia = new Companhia();
+
+                    }
+                }
+            }
+            return _instancia;
+        }
+
+        public string NomesDeCompanhias
+        {
+            get
+            {
+                int r = _random.Next(nomes.Count);
+                return nomes[r].ToString();
+            }
+        }
+    }
+}
---
+++ Aeroporto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aeroporto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Companhia n1 = Companhia.GetCompanhia();
+            Companhia n2 = Companhia.GetCompanhia();
+
+            if(n1 == n2)
+            {
+                Console.WriteLine("Alguma Companhia?\n");
+            }
+
+            Companhia nome = Companhia.GetCompanhia();
+
+            for(int i = 0; i < 5; i++)
+            {
+                string companhia = nome.NomesDeCompanhias;
+                Console.WriteLine("Nome da " + (i + 1) + "° companhia: " + companhia);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
